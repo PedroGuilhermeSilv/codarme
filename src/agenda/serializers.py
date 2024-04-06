bc704ee1fe1a3d6be5b9162fb6babaf1eea94300
guilherme <vinespedro55@gmail.com>
2024-04-06 12:02:18 +0000

    feat: :sparkles: Update settings.json, README.md, serializers.py, and views.py

--- src/agenda/serializers.py
+++ src/agenda/serializers.py
@@ -6,3 +6,10 @@ class AgendamentoSerializer(serializers.Serializer):
     nome_cliente = serializers.CharField(max_length=255)
     email_cliente = serializers.EmailField()
     telefone_cliente = serializers.CharField(max_length=20)
+
+
+class AgendamentoPatchSerializer(serializers.Serializer):
+    data_horario = serializers.DateTimeField(required=False)
+    nome_cliente = serializers.CharField(max_length=255, required=False)
+    email_cliente = serializers.EmailField(required=False)
+    telefone_cliente = serializers.CharField(max_length=20, required=False)
--- src/agenda/views.py
+++ src/agenda/views.py
@@ -1,15 +1,28 @@
 from agenda.models import Agendamento
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
-from .serializers import AgendamentoSerializer
+from .serializers import AgendamentoSerializer, AgendamentoPatchSerializer
 from rest_framework.decorators import api_view
 
 
-@api_view(["GET"])
+@api_view(["GET", "PATCH"])
 def agendamento_detail(request, id):
-    agendamento = get_object_or_404(Agendamento, pk=id)
-    serializer = AgendamentoSerializer(agendamento)
-    return JsonResponse(serializer.data)
+    if request.method == "GET":
+        agendamento = get_object_or_404(Agendamento, id=id)
+        serializer = AgendamentoSerializer(agendamento)
+        return JsonResponse(serializer.data)
+    if request.method == "PATCH":
+        obj = get_object_or_404(Agendamento, id=id)
+        serializer = AgendamentoPatchSerializer(data=request.data, partial=True)
+        if serializer.is_valid():
+            v_data = serializer.validated_data
+            obj.data_horario = v_data.get("data_horario", obj.data_horario)
+            obj.nome_cliente = v_data.get("nome_cliente", obj.nome_cliente)
+            obj.email_cliente = v_data.get("email_cliente", obj.email_cliente)
+            obj.telefone_cliente = v_data.get("telefone_cliente", obj.telefone_cliente)
+            obj.save()
+            return JsonResponse(serializer.data, status=204)
+        return JsonResponse(serializer.errors, status=400)
 
 
 @api_view(["GET", "POST"])
@@ -25,5 +38,5 @@ def agendamento_list(request):
         if serializer.is_valid():
             valide_data = serializer.validated_data
             Agendamento.objects.create(**valide_data)
-            return JsonResponse( status=201)
+            return JsonResponse(valide_data, status=201)
         return JsonResponse(serializer.errors, status=400)
