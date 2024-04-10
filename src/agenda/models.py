6dfa228e4cdbc32ccbede701260045516694faf7
guilherme <vinespedro55@gmail.com>
2024-04-10 14:00:19 -0300

    feat: :test_tube: add test list

--- src/agenda/models.py
+++ src/agenda/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Agendamento(models.Model):
     data_horario = models.DateTimeField()
     nome_cliente = models.CharField(max_length=255)
--- src/agenda/tests.py
+++ src/agenda/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from rest_framework.test import APIClient
+from agenda.models import Agendamento
+import pytest
 
-# Create your tests here.
+
+@pytest.fixture
+def agendamento() -> Agendamento:
+    return Agendamento.objects.create(
+        data_horario="2021-10-10T10:00:00Z",
+        nome_cliente="Fulano",
+        email_cliente="pedro@hotmail.com",
+        telefone_cliente="11999999999",
+    )
+
+
+@pytest.mark.django_db
+class TestsListAgendamentos:
+    def test_list_agendamentos(self):
+        client = APIClient()
+        response = client.get("/api/agendamentos/")
+        assert response.status_code == 200
+        data = response.json()
+        assert data == []
+
+    def test_listagem_de_agendamentos_criados(self, agendamento):
+        client = APIClient()
+        response = client.get("/api/agendamentos/")
+        assert response.status_code == 200
+        data = response.json()
+        assert len(data) == 1
+        assert data[0]["id"] == agendamento.id
