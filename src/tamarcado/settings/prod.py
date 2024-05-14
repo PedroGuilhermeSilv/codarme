ec86b80e9ec8296b3aea027153b1978b2089ad08
guilherme <vinespedro55@gmail.com>
2024-05-14 17:04:34 -0300

    chore: Update production database settings to use SQLite3

--- src/tamarcado/settings/prod.py
+++ src/tamarcado/settings/prod.py
@@ -3,21 +3,25 @@ from src.tamarcado.settings.base import *  # noqa
 import os
 
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB", "my_db"),
-        "USER": os.getenv("POSTGRES_USER", "my_user"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "my_password"),
-        "HOST": os.getenv("HOST", "db"),
-        "PORT": os.getenv("PORT", "5432"),
+DATABASES = (
+    {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",  # noqa
+        }
     }
-} if not TESTING else {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",  # noqa
+    if TESTING
+    else {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("POSTGRES_DB", "my_db"),
+            "USER": os.getenv("POSTGRES_USER", "my_user"),
+            "PASSWORD": os.getenv("POSTGRES_PASSWORD", "my_password"),
+            "HOST": os.getenv("HOST", "db"),
+            "PORT": os.getenv("PORT", "5432"),
+        }
     }
-}
+)
 
 ALLOWED_HOSTS = ["*"]
 
